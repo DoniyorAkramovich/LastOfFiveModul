778005452a1f37400b81142e8d5315a577453209
DoniyorAkramovich <berd.doniyor@gmail.com>
2022-06-24 15:06:40 +0500

    Initial Commit

---
+++ LastOfFive/LastOfFiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  LastOfFiveApp.swift
+//  LastOfFive
+//
+//  Created by Doniyor on 24/06/22.
+//
+
+import SwiftUI
+
+@main
+struct LastOfFiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
